849247cb18b339e23f0d9fd32f039a01e0022cac
Leo Louis <leo.louis@yahoo.com>
2026-07-27 00:33:17 +0530

    Add PrismaModule to AppModule imports

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    PrismaModule,
+  ],
 })
 export class AppModule {}
