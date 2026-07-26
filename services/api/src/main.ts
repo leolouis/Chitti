eae2567ce2fb6ea4ea4dc4410986be9f930ca2a2
Leo Louis <leo.louis@yahoo.com>
2026-07-27 00:30:17 +0530

    Initialize NestJS application with CORS enabled

---
+++ services/api/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  await app.listen(3001);
+
+  console.log(
+    'Chitti API running on http://localhost:3001'
+  );
+}
+
+bootstrap();
