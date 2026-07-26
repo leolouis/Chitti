ca717c951f3fa581b884569fe7d853bbeeb794bc
Leo Louis <leo.louis@yahoo.com>
2026-07-27 00:43:38 +0530

    Add OrganizationsModule to app module imports

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 
 @Module({
@@ -18,6 +19,8 @@ import { UsersModule } from './users/users.module';
     AuthModule,
 
     UsersModule,
+
+    OrganizationsModule,
   ],
 
   controllers: [],
