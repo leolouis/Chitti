cbe74ec79f7f9fe28f7046719f6dd9aac1f93e24
Leo Louis <leo.louis@yahoo.com>
2026-07-27 01:22:30 +0530

    Implement shutdown hooks in PrismaService
    Added enableShutdownHooks method to handle app shutdown gracefully.

--- services/api/src/prisma.service.ts
+++ services/api/src/prisma.service.ts
@@ -1,16 +1,37 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+
 import { PrismaClient } from '@prisma/client';
+
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+
   async onModuleInit() {
     await this.$connect();
   }
 
+
   async onModuleDestroy() {
     await this.$disconnect();
   }
+
+
+  async enableShutdownHooks(app: any) {
+
+    process.on(
+      'beforeExit',
+      async () => {
+        await app.close();
+      },
+    );
+
+  }
+
 }
